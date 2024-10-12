75e5b1a3051d82471db080856dc2d94b44645540
Robert Valera <valeraeduardo1198@gmail.com>
2024-10-12 01:17:01 -0500

    Cambios 12 octubre 2024

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const slider = document.querySelector("#slider");
 let sliderSection = document.querySelectorAll(".slider__section");
 let sliderSectionLast = sliderSection[sliderSection.length -1];
